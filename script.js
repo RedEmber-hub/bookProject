ecc8ada46928c302e67a41fc35901844a72a6fb8
RedEmber-hub <redember38@gmail.com>
2025-04-18 15:57:12 +0300

    сортировка книг по номеру в заголовке

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 document.querySelector('.adv').remove(); // удаление рекламы
+
+const booksContainer = document.querySelector('.books'); // контейнер для книг
+const arraybooks = Array.from(document.querySelectorAll(".book")); // массив книг
+
+//сортировка по номеру в заголовке книги
+arraybooks.sort((a, b) => {
+    const bookA = +a.querySelector('a').textContent.match(/\d+/)[0];
+    const bookB = +b.querySelector('a').textContent.match(/\d+/)[0];
+
+    return bookA - bookB;
+});
+
+// возвращаем отсортированную стопку книг в контейнер
+arraybooks.forEach(book => booksContainer.appendChild(book)); 
